2639c90792f5022debdba4ed3a8272b5c68c6d3e
mhassan35 <mhassan909234@gmail.com>
2025-02-21 21:34:31 +0500

    removed errors

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -30,7 +30,7 @@ export default function Login() {
             Welcome Back
           </h2>
           <p className="mt-2 text-center text-sm text-gray-400">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Link href="/signup" className="text-blue-400 hover:text-blue-300">
               Sign up
             </Link>
